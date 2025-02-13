b01565999110db55ed8c08414c0891a0364d5302
riddickkakati <riddick.kakati@iitg.ac.in>
2025-02-13 19:41:46 +0530

    from air2water to aqualite engine

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -6,9 +6,9 @@ function Header() {
   return (
     <div className="header">
         <img src={logo} alt="BWF logo" height="150"/>
-        <p> <a href="https://youtu.be/q3dYKgsLWFU?si=-kFGLCtDIjOTN-E6" target="_blank" rel="noopener noreferrer">Link to working demonstration</a></p>
+        <p> <a href="https://youtu.be/Wz9TVSZ7Ze0?si=KzuIlUX4R6hGUsji" target="_blank" rel="noopener noreferrer">Link to working demonstration</a></p>
         <p>An improved <a href="https://doi.org/10.4081/aiol.2016.5791" target="_blank" rel="noopener noreferrer">air2water model</a></p>
-        <p>A full stack scientific model development project by <a href="https://iitg.ac.in/stud/riddick.kakati/index.html" target="_blank" rel="noopener noreferrer">Dr. Riddick Kakati</a></p>
+        <p>A full stack scientific model development project by <a href="https://riddickkakati.github.io/" target="_blank" rel="noopener noreferrer">Dr. Riddick Kakati</a></p>
     </div>
   );
 }
